372cef5a98fc112354b415db9dad5f30f2abc1a7
Lucas Oliveira <127795945+loliveir4@users.noreply.github.com>
2025-09-04 19:53:32 +0100

    feat: addin data access

---
+++ Src/BackEnd/MyRecipeBook.Infrastructure/DataAccess/MyRecipeBookDbCobtext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAccess;
+
+public class MyRecipeBookDbCobtext : DbContext
+{
+    public MyRecipeBookDbCobtext(DbContextOptions<MyRecipeBookDbCobtext> options) : base(options) { }
+    
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyRecipeBookDbCobtext).Assembly);
+    }
+}
---
+++ Src/BackEnd/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Repositories.Users;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+public class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepository
+{
+    private readonly MyRecipeBookDbCobtext _dbcontext;
+
+    public UserRepository(MyRecipeBookDbCobtext dbcontext) => _dbcontext = dbcontext;
+
+    public async Task Add(User user) => await _dbcontext.Users.AddAsync(user);
+
+    public async Task<bool> ExistActiveUserWithEmail(string email) => await _dbcontext.Users.AnyAsync(u => u.Email == email && u.Active);
+
+}
